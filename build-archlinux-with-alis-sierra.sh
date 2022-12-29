9652accd35d5ebf01f28ce9b0f020380a3dba5ff
Erik Dubois <erik.dubois@gmail.com>
2022-12-29 08:09:51 +0100

    new archiso

--- build-archlinux-with-alis-sierra.sh
+++ build-archlinux-with-alis-sierra.sh
@@ -38,7 +38,7 @@ echo
 	isoLabel='archlinux-'$(date +%Y.%m.%d)'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 68-1"
+	archisoRequiredVersion="archiso 69-1"
 	buildFolder=$HOME"/Sierra-build"
 	outFolder=$HOME"/Sierra-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
